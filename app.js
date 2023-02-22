df84b63eac48fbe0cd34470fe09691e5d45064d1
Ibrahim Khalil <ibrahimkhalil@Ibrahims-MacBook-Air.local>
2023-02-23 00:32:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,281 @@
+const express=require("express");
+const app=express();
+const port=process.env.PORT || 3000;
+const bodyParser=require("body-parser");
+const fs = require('fs');
+const content = `{"hello":"aa"}`;
+const ejs=require("ejs");
+const mongoose = require('mongoose');
+app.set('view engine', 'ejs');
+
+// Mongoose configuration
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb://127.0.0.1:27017/tedx", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Declare a new mongoose schema for database
+const schema = new mongoose.Schema({
+    _id:String,
+    teamMembers:String,
+    pocName:String,
+    pocContact:String,
+    code:Number,
+    clues:[Number],
+  },{ timestamps: true });
+
+  const clues = new mongoose.Schema({
+    _id:Number,
+    code:String,
+    clue:String,
+  });
+
+// Create a model following the defined schema
+const Team = mongoose.model('Team', schema);
+const Clue = mongoose.model('Clue', clues);
+
+// Clue.create(
+//     {
+//         _id:0,
+//         code:"",
+//         clue:""
+//     }, function(err,res){
+//         console.log(res);
+//     });
+// Clue.create([
+// {
+//     _id:1,
+//     code:"BdHjiNiTWVWo",
+//     clue:"Clue 1"
+// },
+// {
+//     _id:2,
+//     code:"UQMWkNTRIUcB",
+//     clue:"Clue 2"
+// },
+// {
+//     _id:3,
+//     code:"g4tfyyFhASHh",
+//     clue:"Clue 3"
+// },
+// {
+//     _id:4,
+//     code:"6uur9sVAjgKx",
+//     clue:"Clue 4"
+// },
+// {
+//     _id:5,
+//     code:"F8ugKiBGD2hZ",
+//     clue:"Clue 5"
+// },
+// {
+//     _id:6,
+//     code:"hd2SBkwrgizb",
+//     clue:"Clue 6"
+// },
+// {
+//     _id:7,
+//     code:"UbCar8q9fyua",
+//     clue:"Clue 7"
+// },
+// {
+//     _id:8,
+//     code:"qrm0UOmzIjlO",
+//     clue:"Clue 8"
+// },
+// {
+//     _id:9,
+//     code:"uB6DRucMLKTp",
+//     clue:"Clue 9"
+// },
+// {
+//     _id:10,
+//     code:"t7mxN0nUENU6",
+//     clue:"Clue 10"
+// },
+// ]).then((result => {
+//     if(result) {
+//         console.log("clues added");
+//     }
+// })).catch(err => {
+//     if(err) console.log(err); 
+// });
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port, function(req,res){
+    console.log("listening on port "+ port)
+});
+
+const options = {
+    dotfiles: 'ignore',
+    etag: false,
+    extensions: ['htm', 'html'],
+    index: false,
+    maxAge: '1d',
+    redirect: false,
+    setHeaders (res, path, stat) {
+      res.set('x-timestamp', Date.now())
+    }
+  }
+
+  
+app.use('/',express.static(__dirname+"/public",options));
+
+app.get("/",function(req,res){
+    res.render("index");
+});
+
+app.get("/about",function(req,res){
+    res.render("about");
+});
+
+app.get("/speakers",function(req,res){
+    res.render("speakers");
+});
+
+app.get("/event",function(req,res){
+    res.render("event");
+});
+
+app.get("/partners",function(req,res){
+    res.render("sponsors");
+});
+
+app.get("/register",function(req,res){
+    res.render("registration");
+});
+
+
+app.get("/clues/:number",function(req,res){
+        if(req.params.number==0){
+            Team.find({},function(err,results){
+                if(err){
+                    console.log(err);
+                }else{
+                    res.render("clues",{json:results,number:0,query:req.query.code})
+                }
+            });
+        }else if(req.params.number<=10){
+        Team.find({clues:req.params.number-1},function(err,results){
+            if(err){
+                console.log(err);
+            }else{
+                res.render("clues",{json:results,number:req.params.number,query:req.query.code})
+            }
+        });
+       }else{
+        res.render("clues",{json:{},number:"Too big bro! Not gonna say what",query:req.query.code})
+       } 
+});
+
+app.post("/clues/:number",function(req,res){
+    Team.findOne({_id:req.body.team,code:req.body.code},function(err,results){
+        if(err){
+            console.log(err);
+        }else{
+            if(results!=null){
+                if(req.params.number==0){
+                    var updated=results.clues;
+                    updated.includes(parseInt(req.params.number))?console.log("already there"):updated.push(parseInt(req.params.number));
+                    Team.updateOne({_id:req.body.team},{clues:updated},function(err,resu){
+                        if(err){
+                            console.log(err);
+                        } else{
+                            Clue.findOne({_id:1},function(err,baby){
+                                if(!err){
+                                    console.log(baby);
+                                    res.render("indclue",{clueText:baby.clue});
+                                }
+                            });             
+                        }
+                    });
+                }else{
+                    var updated=results.clues;
+                    Team.find({clues:parseInt(req.params.number)-1},function(err,results){
+                        if(err){
+                            console.log(err);
+                        }else{
+                            if(results.length!=0){
+                              Clue.find({_id:parseInt(req.params.number),code:req.query.code},function(err,rp){
+                                if(rp.length!=0){
+                                    updated.includes(parseInt(req.params.number))?console.log("already there"):updated.push(parseInt(req.params.number));
+                                    Team.updateOne({_id:req.body.team},{clues:updated},function(err,resu){
+                                        if(err){
+                                            console.log(err);
+                                        } else{
+                                            var numb=parseInt(req.params.number)+1;
+                                            console.log(numb);
+                                            if(numb>10){
+                                                res.render("indclue",{clueText:"Congratulations. Now concentrate on studies."});
+
+                                            }else{
+                                            Clue.findOne({_id:numb},function(err,baby){
+                                                if(!err){
+                                                console.log(req.params.number);
+                                                console.log(baby);
+                                                res.render("indclue",{clueText:baby.clue});
+                                                }
+                                            });
+                                        }             
+
+                                        }
+                                    });
+                                }else{
+                                    res.render("indclue",{clueText:"ABHAHAHAHHA NO BRO REACH THE CLUE FIRST AND SCAN THE CODE CORRECTLY"});
+                                }
+                            });
+
+                            } else{
+                                res.send("NO!");
+                            }
+                            
+                       }
+                    });
+                }
+            }else{
+                res.send("incorrect authentication code")
+            }
+        }
+    });
+});
+
+app.post("/register",function(req,res){
+    teamName=req.body.name;
+    teamMembers=req.body.members;
+    pocName=req.body.poc;
+    pocContact=req.body.pocContact;
+    code=Math.floor(100000 + Math.random() * 900000);
+    var updatedAt=new Date();
+    var clues=[];
+    Team.find({_id:teamName},function(err,results){
+        if(results.length!=0){
+            res.send("Team already registered!");
+        } else{
+            Team.create({
+                _id:teamName,
+                teamMembers:teamMembers,
+                pocName:pocName,
+                pocContact:pocContact,
+                code:code,
+                clues:[]
+            }).then((result => {
+                if(result) {
+                    setTimeout(() => {
+                        res.render("registration");
+                    }, 1000)
+                }
+            })).catch(err => {
+                if(err) console.log(err); res.send("Error in creating document. \n"+err);
+            });
+        }
+    });
+});
+app.get("*",function(req,res){
+    res.render("404");
+});
+
---
+++ public/assets/js/main.js
@@ -0,0 +1,347 @@
+/**
+* Template Name: TheEvent - v3.0.0
+* Template URL: https://bootstrapmade.com/theevent-conference-event-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+!(function($) {
+  "use strict";
+
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+    
+  //Typewrite
+  var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 8) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 150 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
+
+  //Counter
+var counted = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (counted == 0 && $(window).scrollTop() > oTop) {
+    $('.count').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    counted = 1;
+  }
+
+});
+    
+  // Header fixed on scroll
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
+
+  // Initialize Venobox
+  $(window).on('load', function() {
+    $('.venobox').venobox({
+      bgcolor: '',
+      overlayColor: 'rgba(6, 12, 34, 0.85)',
+      closeBackground: '',
+      closeColor: '#fff',
+      share: false
+    });
+  });
+
+  // Initiate superfish on nav menu
+  $('.nav-menu').superfish({
+    animation: {
+      opacity: 'show'
+    },
+    speed: 400
+  });
+
+  // Mobile Navigation
+  if ($('#nav-menu-container').length) {
+    var $mobile_nav = $('#nav-menu-container').clone().prop({
+      id: 'mobile-nav'
+    });
+    $mobile_nav.find('> ul').attr({
+      'class': '',
+      'id': ''
+    });
+    $('body').append($mobile_nav);
+    $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
+    $('body').append('<div id="mobile-body-overly"></div>');
+    $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
+
+    $(document).on('click', '.menu-has-children i', function(e) {
+      $(this).next().toggleClass('menu-item-active');
+      $(this).nextAll('ul').eq(0).slideToggle();
+      $(this).toggleClass("fa-chevron-up fa-chevron-down");
+    });
+
+    $(document).on('click', '#mobile-nav-toggle', function(e) {
+      $('body').toggleClass('mobile-nav-active');
+      $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+      $('#mobile-body-overly').toggle();
+    });
+
+    $(document).click(function(e) {
+      var container = $("#mobile-nav, #mobile-nav-toggle");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('#mobile-body-overly').fadeOut();
+        }
+      }
+    });
+  } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
+    $("#mobile-nav, #mobile-nav-toggle").hide();
+  }
+
+  // Smooth scroll for the navigation menu and links with .scrollto classes
+  var scrolltoOffset = $('#header').outerHeight() - 21;
+  if (window.matchMedia("(max-width: 991px)").matches) {
+    scrolltoOffset += 20;
+  }
+  $(document).on('click', '.nav-menu a, #mobile-nav a, .scrollto', function(e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+        e.preventDefault();
+
+        var scrollto = target.offset().top - scrolltoOffset;
+
+        if ($(this).attr("href") == '#header') {
+          scrollto = 0;
+        }
+
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.nav-menu').length) {
+          $('.nav-menu .menu-active').removeClass('menu-active');
+          $(this).closest('li').addClass('menu-active');
+        }
+
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('#mobile-body-overly').fadeOut();
+        }
+        return false;
+      }
+    }
+  });
+
+  // Activate smooth scroll on page load with hash links in the url
+  $(document).ready(function() {
+    if (window.location.hash) {
+      var initial_nav = window.location.hash;
+      if ($(initial_nav).length) {
+        var scrollto = $(initial_nav).offset().top - scrolltoOffset;
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
+
+  // Navigation active state on scroll
+  var nav_sections = $('section');
+  var main_nav = $('.nav-menu, #mobile-nav');
+
+  $(window).on('scroll', function() {
+    var cur_pos = $(this).scrollTop() + 200;
+
+    nav_sections.each(function() {
+      var top = $(this).offset().top,
+        bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        if (cur_pos <= bottom) {
+          main_nav.find('li').removeClass('menu-active');
+        }
+        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('menu-active');
+      }
+      if (cur_pos < 300) {
+        $(".nav-menu li:first").addClass('menu-active');
+      }
+    });
+  });
+
+  // Gallery carousel (uses the Owl Carousel library)
+  $(".gallery-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    center: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 3
+      },
+      992: {
+        items: 4
+      },
+      1200: {
+        items: 5
+      }
+    }
+  });
+
+  // Buy tickets select the ticket type on click
+  $('#buy-ticket-modal').on('show.bs.modal', function(event) {
+    var button = $(event.relatedTarget);
+    var ticketType = button.data('ticket-type');
+    var modal = $(this);
+    modal.find('#ticket-type').val(ticketType);
+  });
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
+
+})(jQuery);
+
+// var countDownDate = new Date("February 17, 2023 00:00:00").getTime();
+// var x = setInterval(function(){
+//   var now = new Date().getTime();
+//   var distance = new countDownDate - now;
+
+//   var days = Math.floor(distance /(1000 * 60 * 60 *24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+// document.getElementById("day").innerHTML = days;
+// document.getElementById("hour").innerHTML = hours;
+// document.getElementById("minute").innerHTML = minutes;
+// },1000);
+// document.getElementById("day").innerHTML = days;
+// document.getElementById("hour").innerHTML = hours;
+// document.getElementById("minute").innerHTML = minutes;
+
+
+var countDownDate = new Date("18 feb, 2023 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("day").innerHTML = days;
+  document.getElementById("hour").innerHTML = hours;
+  document.getElementById("minute").innerHTML = minutes;
+  document.getElementById("second").innerHTML = seconds;
+
+  // If the count down is finished, write some text
+  if (countDownDate < now) {
+    clearInterval(x);  
+    document.getElementById("timer").innerHTML = '<div class="section-header"><h2 class="mt-5">Event has begun</h2></div>';
+  }
+}, 1000);
